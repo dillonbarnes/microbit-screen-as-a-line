70b19e59259aaa9285ff0915a2724766b652ad9b
dillonbarnes <59509438+dillonbarnes@users.noreply.github.com>
2020-05-16 16:24:09 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
-for(let i = 0; i < 25; ++i) {
-    Plotty.TurnOnAnLED(i)
-    basic.pause(100)
-}
-basic.clearScreen()
+//for(let i = 0; i < 25; ++i) {
+//    Plotty.TurnOnAnLED(i)
+//    basic.pause(100)
+//}
+//basic.clearScreen()
 Plotty.TurnOnAnLED(6)
