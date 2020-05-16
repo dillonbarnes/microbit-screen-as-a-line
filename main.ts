fd4437b99c53be95e967f194b8301cabbedcb8a4
dillonbarnes <59509438+dillonbarnes@users.noreply.github.com>
2020-05-16 16:14:23 +0100

    Fixed a small bug.

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@ namespace Plotty {
     //% blockID=All LED's
     //% block="Turn on an LED $index"
     //% index.min=0 index.max=25
-    export function TurnOnLEDs(index: number): void {
+    export function TurnOnAnLED(index: number): void {
     index |= 0
     const y =Math.floor(index / 5);
     const x = Math.floor(index % 5);
