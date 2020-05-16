ca09440d57c11d9815547dfabd273524eb65f85b
dillonbarnes <59509438+dillonbarnes@users.noreply.github.com>
2020-05-16 16:24:32 +0100

    Fixed syntax

--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ namespace Plotty {
     //% index.min=0 index.max=24
     export function TurnOnAnLED(index: number): void {
     index |= 0
-    const y =Math.floor(index / 5);
+    const y = Math.floor(index / 5);
     const x = Math.floor(index % 5);
     led.plot(x,y)
     }
