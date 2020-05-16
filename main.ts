236c92560a162c21e08b98989632e859d14a9aab
dillonbarnes <59509438+dillonbarnes@users.noreply.github.com>
2020-05-16 16:20:23 +0100

    Fixed selection bug.

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ namespace Plotty {
      */
     //% blockId=Plot LED
     //% block="plot at $index"
-    //% index.min=0 index.max=25
+    //% index.min=0 index.max=24
     export function plotAt(index: number): void {
         index |= 0
         const y = Math.floor(index / 5);
@@ -16,7 +16,7 @@ namespace Plotty {
      */
     //% blockID=All LED's
     //% block="Turn on an LED $index"
-    //% index.min=0 index.max=25
+    //% index.min=0 index.max=24
     export function TurnOnAnLED(index: number): void {
     index |= 0
     const y =Math.floor(index / 5);
